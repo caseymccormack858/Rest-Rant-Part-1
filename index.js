8d5040fc6e7419f2fa5daafdd912d28e151fe5be
Casey McCormack <caseymccormack858@gmail.com>
2023-09-04 05:22:30 -0700

    updated

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
 
-require('dotenv').config()
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
-
-app.listen(process.env.PORT)
-
-require('dotenv').config()
-const express = require('express')
-const app = express()
+app.use('/places', require('./controllers/places'));
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+  res.send('Hello world!');
+});
 
 app.get('*', (req, res) => {
-    res.status(404) .send('<h1>404 Page</h1>')
-    
-})
+  res.status(404).send('<h1>404 Page</h1>');
+});
 
-app.listen(process.env.PORT)
+app.listen(3000, () => {
+  console.log('Server is running on port 3001');
+});
